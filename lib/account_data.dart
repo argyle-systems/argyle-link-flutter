31ed87ce210b9fc03a809a7546236d8f2138de45
Mindaugas Varkalys <mindaugas.varkalys@argyle.com>
2023-04-06 13:59:57 +0300

    Create Flutter bindings for Android Link 5

---
+++ lib/account_data.dart
@@ -0,0 +1,11 @@
+class AccountData {
+  final String accountId;
+  final String userId;
+  final String itemId;
+
+  AccountData({
+    required this.accountId,
+    required this.userId,
+    required this.itemId,
+  });
+}
---
+++ lib/ui_event.dart
@@ -0,0 +1,6 @@
+class UIEvent {
+  final String name;
+  final Map<String, dynamic>? properties;
+
+  UIEvent({required this.name, this.properties});
+}
